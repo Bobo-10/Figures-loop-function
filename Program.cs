eb0a881cb3c0e7dd7262682f2489e91a17580b03
Bobo-10 <78552910+Bobo-10@users.noreply.github.com>
2021-02-04 21:35:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height your triangle:");
+            int height = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}    
