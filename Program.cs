25585179746c63d4e83daa7508a404e776a40746
Bobo-10 <78552910+Bobo-10@users.noreply.github.com>
2021-02-04 22:14:19 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ namespace MyFirstRepo
     class Program
     {
         static void Main(string[] args)
+            
+            //перевернутий трикутник
         {
             Console.WriteLine("Enter the height your triangle:");
             int height = int.Parse(Console.ReadLine());
@@ -17,5 +19,42 @@ namespace MyFirstRepo
                 Console.WriteLine();
             }
         }
+        
+        //max значення
+        {
+            int a = 5;
+            int b = 6;
+            int c = 8;
+            int d = 15;
+            int max = a;
+            if (max<b)
+            {
+                max = b;              
+                if (max < c)
+                    max = c;
+             }
+            if (max < d)
+                max = d;
+                    {
+                Console.WriteLine(max);
+            }
+        }
+        //ялинка красива
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter number of triangles of your tree: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    string branch = new String('*', j);
+                    Console.WriteLine(branch.PadLeft(n + 3) + "*" + branch);
+                }
+            }
+
+        
     }
-}    
+}  
+
